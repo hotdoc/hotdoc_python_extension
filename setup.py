e921f576c34eae7a3fa9a4389507109c9464aba2
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 15:35:33 +0100

    Initial import from hotdoc

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "hotdoc_python_extension",
+    version = "0.6",
+    keywords = "python ast sphinx napoleon hotdoc",
+    url='https://github.com/hotdoc/hotdoc_python_extension',
+    author_email = 'mathieu.duponchelle@opencreed.com',
+    license = 'LGPL',
+    description = ("An extension for hotdoc that parses python using the"
+        " standard ast module and napoleon from sphinx"),
+    author = "Mathieu Duponchelle",
+    packages = find_packages(),
+    entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_python_extension.python_extension:get_extension_classes'},
+    install_requires = [
+        'hotdoc>=0.6',
+        'sphinx>=1.3.3',
+    ]
+)
