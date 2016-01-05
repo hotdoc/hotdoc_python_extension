9fec0c036f6e0da4f8981bc236af19701a999d50
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-06 00:34:01 +0100

    0.6.6.1 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "hotdoc_python_extension",
-    version = "0.6.6",
+    version = "0.6.6.1",
     keywords = "python ast sphinx napoleon hotdoc",
     url='https://github.com/hotdoc/hotdoc_python_extension',
     author_email = 'mathieu.duponchelle@opencreed.com',
@@ -11,6 +11,9 @@ setup(
         " standard ast module and napoleon from sphinx"),
     author = "Mathieu Duponchelle",
     packages = find_packages(),
+    package_data = {
+        '': ['*.html'],
+    },
     entry_points = {'hotdoc.extensions': 'get_extension_classes = hotdoc_python_extension.python_extension:get_extension_classes'},
     install_requires = [
         'sphinx>=1.3.3',
